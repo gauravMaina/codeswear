9e5db1a201d7a83e3478a4faae0d052d0eb8e2de
Gaurav Jadhav <you@example.com>
2023-02-01 14:50:35 +0530

    Navbar index and footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
+import Navbar from 'Components/Navbar'
+import Footer from 'Components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,8 +14,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Hi this is me
-      <p className='mx-6 bg-red-500'>this is h1</p>
+     <Navbar/>
+     <Footer/>
     </>
   )
 }
