8cbde98b734c6ff175a3b96c5440b610af3d908b
Gaurav Jadhav <you@example.com>
2023-02-01 16:55:59 +0530

    Designing Tshirt page

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -4,19 +4,19 @@ import Link from 'next/link'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 const Nabvar = () => {
     return (
-        <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2'>
+        <div className='flex flex-col md:flex-row md:justify-start justify-center items-center  py-3 shadow-md'>
             <div className="logo mx-5">
-                <Image src="/vercel.svg" width={100} height={100} />
+                <Image src="/vercel.svg" width={100} height={100} alt='Vercel Image' />
             </div>
             <div className="nav">
-                <ul className='flex items-center space-x-2 font-bold md:text-xl'>
-                    <Link href={'/'}><li>Tshirt</li></Link>
-                    <Link href={'/'}><li>Hoodies</li></Link>
-                    <Link href={'/'}><li>Stickers</li></Link>
-                    <Link href={'/'}><li>Mugs</li></Link>
+                <ul className='flex items-center space-x-6 font-bold md:text-md'>
+                    <Link href={'/tshirt'}><li>Tshirt</li></Link>
+                    <Link href={'/hoodies'}><li>Hoodies</li></Link>
+                    <Link href={'/stickers'}><li>Stickers</li></Link>
+                    <Link href={'/mugs'}><li>Mugs</li></Link>
                 </ul>
             </div>
-            <div className="cart absolute right-0 top-4">
+            <div className="cart absolute right-0 top-4 pr-7">
                 <AiOutlineShoppingCart className='text-xl md:text-3xl' />
             </div>
         </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Footer from 'Components/Footer'
+import Nabvar from 'Components/Navbar'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App ({ Component, pageProps }) {
+  return (
+    <>
+      <Nabvar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
