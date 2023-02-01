5838b5fd024acd0d9c3abbbcbfb38a036aadd893
Gaurav Jadhav <you@example.com>
2023-02-01 17:04:54 +0530

    Designing mugs,stickers and hoodies

--- src/pages/mugs.js
+++ src/pages/mugs.js
@@ -1,8 +1,183 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Mugs() {
   return (
-    <div>Mugs</div>
+    <div><section className='text-gray-600 body-font'>
+    <div className='container px-5 py-24 mx-auto'>
+      <div className='flex flex-wrap -m-4'>
+        <div className='lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5'>
+          <Link href={'/product/Tshirt'}>
+            <a className='object-cover m-auto md:mx-0 h-[30vh] md:h-[36vh] block'>
+              <img
+                alt='ecommerce'
+                className='object-cover object-center w-[50vw]  md:w-full h-full block'
+                src='https://m.media-amazon.com/images/I/61Wnm1KClcL._SX522_.jpg'
+              />
+            </a>
+            <div className='mt-4'>
+              <h3 className='text-gray-500 text-xs tracking-widest title-font mb-1'>
+                T-Shirts
+              </h3>
+              <h2 className='text-gray-900 title-font text-lg font-medium'>
+                Wear the code
+              </h2>
+              <p className='mt-1'>₹449.00</p>
+              <p className='mt-1'>S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className='lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5'>
+          <Link href={'/product/Tshirt'}>
+            <a className='m-auto md:mx-0 h-[30vh] md:h-[36vh] block'>
+              <img
+                alt='ecommerce'
+                className='object-cover object-center w-[50vw]  md:w-full h-full block'
+                src='https://m.media-amazon.com/images/I/61Wnm1KClcL._SX522_.jpg'
+              />
+            </a>
+            <div className='mt-4'>
+              <h3 className='text-gray-500 text-xs tracking-widest title-font mb-1'>
+                T-Shirts
+              </h3>
+              <h2 className='text-gray-900 title-font text-lg font-medium'>
+                Wear the code
+              </h2>
+              <p className='mt-1'>₹449.00</p>
+              <p className='mt-1'>S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className='lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5'>
+          <Link href={'/product/Tshirt'}>
+            <a className='m-auto md:mx-0 h-[30vh] md:h-[36vh] block'>
+              <img
+                alt='ecommerce'
+                className='object-cover object-center w-[50vw]  md:w-full h-full block'
+                src='https://m.media-amazon.com/images/I/61Wnm1KClcL._SX522_.jpg'
+              />
+            </a>
+            <div className='mt-4'>
+              <h3 className='text-gray-500 text-xs tracking-widest title-font mb-1'>
+                T-Shirts
+              </h3>
+              <h2 className='text-gray-900 title-font text-lg font-medium'>
+                Wear the code
+              </h2>
+              <p className='mt-1'>₹449.00</p>
+              <p className='mt-1'>S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className='lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5'>
+          <Link href={'/product/Tshirt'}>
+            <a className='m-auto md:mx-0 h-[30vh] md:h-[36vh] block'>
+              <img
+                alt='ecommerce'
+                className='object-cover object-center w-[50vw]  md:w-full h-full block'
+                src='https://m.media-amazon.com/images/I/61Wnm1KClcL._SX522_.jpg'
+              />
+            </a>
+            <div className='mt-4'>
+              <h3 className='text-gray-500 text-xs tracking-widest title-font mb-1'>
+                T-Shirts
+              </h3>
+              <h2 className='text-gray-900 title-font text-lg font-medium'>
+                Wear the code
+              </h2>
+              <p className='mt-1'>₹449.00</p>
+              <p className='mt-1'>S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className='lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5'>
+          <Link href={'/product/Tshirt'}>
+            <a className='m-auto md:mx-0 h-[30vh] md:h-[36vh] block'>
+              <img
+                alt='ecommerce'
+                className='object-cover object-center w-[50vw]  md:w-full h-full block'
+                src='https://m.media-amazon.com/images/I/61Wnm1KClcL._SX522_.jpg'
+              />
+            </a>
+            <div className='mt-4'>
+              <h3 className='text-gray-500 text-xs tracking-widest title-font mb-1'>
+                T-Shirts
+              </h3>
+              <h2 className='text-gray-900 title-font text-lg font-medium'>
+                Wear the code
+              </h2>
+              <p className='mt-1'>₹449.00</p>
+              <p className='mt-1'>S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className='lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5'>
+          <Link href={'/product/Tshirt'}>
+            <a className='m-auto md:mx-0 h-[30vh] md:h-[36vh] block'>
+              <img
+                alt='ecommerce'
+                className='object-cover object-center w-[50vw]  md:w-full h-full block'
+                src='https://m.media-amazon.com/images/I/61Wnm1KClcL._SX522_.jpg'
+              />
+            </a>
+            <div className='mt-4'>
+              <h3 className='text-gray-500 text-xs tracking-widest title-font mb-1'>
+                T-Shirts
+              </h3>
+              <h2 className='text-gray-900 title-font text-lg font-medium'>
+                Wear the code
+              </h2>
+              <p className='mt-1'>₹449.00</p>
+              <p className='mt-1'>S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className='lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5'>
+          <Link href={'/product/Tshirt'}>
+            <a className='m-auto md:mx-0 h-[30vh] md:h-[36vh] block'>
+              <img
+                alt='ecommerce'
+                className='object-cover object-center w-[50vw]  md:w-full h-full block'
+                src='https://m.media-amazon.com/images/I/61Wnm1KClcL._SX522_.jpg'
+              />
+            </a>
+            <div className='mt-4'>
+              <h3 className='text-gray-500 text-xs tracking-widest title-font mb-1'>
+                T-Shirts
+              </h3>
+              <h2 className='text-gray-900 title-font text-lg font-medium'>
+                Wear the code
+              </h2>
+              <p className='mt-1'>₹449.00</p>
+              <p className='mt-1'>S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className='lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5'>
+          <Link href={'/product/Tshirt'}>
+            <a className='m-auto md:mx-0 h-[30vh] md:h-[36vh] block'>
+              <img
+                alt='ecommerce'
+                className='object-cover object-center w-[50vw]  md:w-full h-full block'
+                src='https://m.media-amazon.com/images/I/61Wnm1KClcL._SX522_.jpg'
+              />
+            </a>
+            <div className='mt-4'>
+              <h3 className='text-gray-500 text-xs tracking-widest title-font mb-1'>
+                T-Shirts
+              </h3>
+              <h2 className='text-gray-900 title-font text-lg font-medium'>
+                Wear the code
+              </h2>
+              <p className='mt-1'>₹16.00</p>
+              <p className='mt-1'>S, M, L, XL, XXL</p>
+              449{' '}
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  </section></div>
   )
 }
 
