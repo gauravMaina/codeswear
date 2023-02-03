36520a644dc9584283dc45cac8930a7eb58da0cc
gaurav.jadhav <gaurav.jadhav@octalinfosolution.com>
2023-02-03 18:47:37 +0530

    add ,edit and get Products

---
+++ src/pages/api/getproducts.js
@@ -0,0 +1,12 @@
+import Product from "../../../models/Product"
+import connectDB from "../../../middleware/mongoose"
+import mongoose from "mongoose"
+
+const handler = async (req, res) => {
+    let products = await Product.find()
+
+    res.status(200).json({ products })
+}
+
+mongoose.models = {}
+export default connectDB(handler)
