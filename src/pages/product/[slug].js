be3d93ae32b1200bae0970324982b0b22c246781
gaurav.jadhav <gaurav.jadhav@octalinfosolution.com>
2023-02-06 15:25:43 +0530

    replacing dummy text

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -36,10 +36,9 @@ const Slug = ({ addToCart, product, variants }) => {
         <section className="text-gray-600 body-font overflow-hidden">
             <div className="container px-5 py-14 mx-auto">
                 <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                    <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto px-24 object-cover object-top rounded" src="https://m.media-amazon.com/images/I/613k0gYkdZS._UY879_.jpg" />
-                    <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                    <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto px-24 object-cover object-top rounded" src={product.image} /> <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                         <h2 className="text-sm title-font text-gray-500 tracking-widest">CODESWEAR</h2>
-                        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">Wear the code (XL/Blue)</h1>
+                        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.title} ({product.size}/{product.color})</h1>
                         <div className="flex mb-4">
                             <span className="flex items-center">
                                 <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
@@ -77,7 +76,7 @@ const Slug = ({ addToCart, product, variants }) => {
                                 </a>
                             </span>
                         </div>
-                        <p className="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+                        <p className="leading-relaxed">{product.desc}</p>
 
                         <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                             <div className="flex">
@@ -114,7 +113,7 @@ const Slug = ({ addToCart, product, variants }) => {
                         <div className="flex">
                             <span className="title-font font-medium text-2xl text-gray-900">₹499</span>
                             <button onClick={() => { refreshVariant(size, 'red') }} className="flex ml-8 text-white bg-orange-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-orange-600 rounded">Buy Now</button>
-                            <button onClick={() => { addToCart(slug, 1, 499, 'Wear the code(XL, red)', 'XL', 'red') }} className="flex ml-2 text-white bg-orange-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-orange-600 rounded">Add to cart</button>
+                            <button onClick={() => { addToCart(slug, 1, 499, product.title, product.size, product.color) }} className="flex ml-2 text-white bg-orange-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-orange-600 rounded">Add to cart</button>
                             <button onClick={() => { refreshVariant(size, 'red') }} className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                     <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
--- src/pages/tshirts.js
+++ src/pages/tshirts.js
@@ -28,19 +28,19 @@ const Tshirts = ({ products }) => {
                                             </h2>
                                             <p className="mt-1">Rs {products[item].price}</p>
                                             <div className="mt-1">
-                    {products[item].size.includes('S') && <span className='border border-orange-900 px-1 mx-1'>S </span>}
-                    {products[item].size.includes('M') && <span className='border border-orange-200 px-1 mx-1'>M </span>}
-                    {products[item].size.includes('L') && <span className='border border-orange-200 px-1 mx-1'>L </span>}
-                    {products[item].size.includes('XL') && <span className='border border-orange-200 px-1 mx-1'>XL </span>}
-                    {products[item].size.includes('XXL') && <span className='border border-orange-200 px-1 mx-1'>XXL </span>}
-                  </div>
-                  <div className="mt-1">
-                    {products[item].color.includes('red') && <button className="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {products[item].color.includes('green') && <button className="border-2 border-gray-300 ml-1 bg-green-700 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {products[item].color.includes('yellow') && <button className="border-2 border-gray-300 ml-1 bg-yellow-300 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {products[item].color.includes('blue') && <button className="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {products[item].color.includes('purple') && <button className="border-2 border-gray-300 ml-1 bg-purple-700 rounded-full w-6 h-6 focus:outline-none"></button>}
-                  </div>
+                                                {products[item].size.includes('S') && <span className='border border-orange-200 px-1 mx-1'>S </span>}
+                                                {products[item].size.includes('M') && <span className='border border-orange-200 px-1 mx-1'>M </span>}
+                                                {products[item].size.includes('L') && <span className='border border-orange-200 px-1 mx-1'>L </span>}
+                                                {products[item].size.includes('XL') && <span className='border border-orange-200 px-1 mx-1'>XL </span>}
+                                                {products[item].size.includes('XXL') && <span className='border border-orange-200 px-1 mx-1'>XXL </span>}
+                                            </div>
+                                            <div className="mt-1">
+                                                {products[item].color.includes('red') && <button className="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                                {products[item].color.includes('green') && <button className="border-2 border-gray-300 ml-1 bg-green-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                                {products[item].color.includes('yellow') && <button className="border-2 border-gray-300 ml-1 bg-yellow-300 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                                {products[item].color.includes('blue') && <button className="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                                {products[item].color.includes('purple') && <button className="border-2 border-gray-300 ml-1 bg-purple-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                            </div>
                                         </div>
                                     </div>
                                 </Link>
@@ -63,20 +63,20 @@ export async function getServerSideProps(context) {
     let products = await Product.find({ category: "T-shirt" });
     let tshirts = {}
     for (let item of products) {
-      if (item.titles in tshirts) {
-        if (!tshirts[item.title].color.includes(item.color) && item.availableQty > 0) {
-          tshirts[item.title].color.push(item.color)
+        if (item.titles in tshirts) {
+            if (!tshirts[item.title].color.includes(item.color) && item.availableQty > 0) {
+                tshirts[item.title].color.push(item.color)
+            }
+            if (!tshirts[item.title].size.includes(item.size) && item.availableQty > 0) {
+                tshirts[item.title].size.push(item.size)
+            }
+        } else {
+            tshirts[item.title] = JSON.parse(JSON.stringify(item))
+            if (item.availableQty > 0) {
+                tshirts[item.title].color = [item.color]
+                tshirts[item.title].size = [item.size]
+            }
         }
-        if (!tshirts[item.title].size.includes(item.size) && item.availableQty > 0) {
-          tshirts[item.title].size.push(item.size)
-        }
-      } else {
-        tshirts[item.title] = JSON.parse(JSON.stringify(item))
-        if (item.availableQty > 0) {
-          tshirts[item.title].color = [item.color]
-          tshirts[item.title].size = [item.size]
-        }
-      }
     }
     return {
         props: {
